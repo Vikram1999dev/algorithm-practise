e8d5c965a90c4039c357b20b5eda82742b3a023b
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-11-26 15:49:02 +0530

    success

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-var LinkedNode = /** @class */ (function () {
-    function LinkedNode(data) {
-        this.data = data;
-        this.next = null;
+var maxSumWithWindow = function (arr, windowSize) {
+    var windowSum = 0;
+    for (var i = 0; i < windowSize; i++) {
+        windowSum += arr[i];
     }
-    return LinkedNode;
-}());
-var intersectionLkNde = function (head1, head2) {
-    if (!head1 || !head2) {
-        return null;
+    var maxSum = windowSum;
+    var temp = 0;
+    for (var i = windowSize; i < arr.length; i++) {
+        windowSum = windowSum + arr[i] - arr[i - windowSize];
+        maxSum = Math.max(maxSum, windowSum);
     }
-    var ptr1 = head1;
-    var ptr2 = head2;
-    while (ptr1 !== ptr2) {
-        ptr1 = ptr1.next;
-        ptr2 = ptr2.next;
-        if (ptr1 === ptr2) {
-            return ptr1;
-        }
-        if (!ptr1) {
-            ptr1 = head2;
-        }
-        if (!ptr2) {
-            ptr2 = head1;
-        }
-    }
-    return ptr1;
+    return maxSum;
 };
-// let newNode:LinkedNode|null;
-var head1 = new LinkedNode(1);
-head1.next = new LinkedNode(2);
-head1.next.next = new LinkedNode(3);
-head1.next.next.next = new LinkedNode(4);
-head1.next.next.next.next = new LinkedNode(5);
-head1.next.next.next.next.next = new LinkedNode(6);
-var head2 = new LinkedNode(1.1);
-head2.next = new LinkedNode(2.2);
-head2.next.next = new LinkedNode(3.3);
-head2.next.next.next = new LinkedNode(4.4);
-head2.next.next.next.next = head1.next.next.next;
-console.log(intersectionLkNde(head1, head2));
+console.log(maxSumWithWindow([1, 4, 2, 10, 2, 3, 1, 0, 20], 4));
--- index.ts
+++ index.ts
@@ -1,53 +1,18 @@
-class LinkedNode {
-  data: number;
-  next: LinkedNode | null;
-  constructor(data: number) {
-    this.data = data;
-    this.next = null;
+const maxSumWithWindow = (arr: number[], windowSize: number): number => {
+  let windowSum: number = 0;
+  for (let i: number = 0; i < windowSize; i++) {
+    windowSum += arr[i];
   }
-}
+  let maxSum: number = windowSum;
 
-const intersectionLkNde = (
-  head1: LinkedNode | null,
-  head2: LinkedNode | null,
-): LinkedNode | null => {
-  if (!head1 || !head2) {
-    return null;
+  let temp: number = 0;
+
+  for (let i = windowSize; i < arr.length; i++) {
+    windowSum = windowSum + arr[i] - arr[i - windowSize];
+    maxSum = Math.max(maxSum, windowSum);
   }
 
-  let ptr1: LinkedNode = head1;
-  let ptr2: LinkedNode = head2;
-
-  while (ptr1 !== ptr2) {
-    ptr1 = ptr1.next;
-    ptr2 = ptr2.next;
-    if (ptr1 === ptr2) {
-      return ptr1;
-    }
-    if (!ptr1) {
-      ptr1 = head2;
-    }
-
-    if (!ptr2) {
-      ptr2 = head1;
-    }
-  }
-
-  return ptr1;
+  return maxSum;
 };
 
-// let newNode:LinkedNode|null;
-let head1 = new LinkedNode(1);
-head1.next = new LinkedNode(2);
-head1.next.next = new LinkedNode(3);
-head1.next.next.next = new LinkedNode(4);
-head1.next.next.next.next = new LinkedNode(5);
-head1.next.next.next.next.next = new LinkedNode(6);
-
-let head2 = new LinkedNode(1.1);
-head2.next = new LinkedNode(2.2);
-head2.next.next = new LinkedNode(3.3);
-head2.next.next.next = new LinkedNode(4.4);
-head2.next.next.next.next = head1.next.next.next;
-
-console.log(intersectionLkNde(head1, head2));
+console.log(maxSumWithWindow([1, 4, 2, 10, 2, 3, 1, 0, 20], 4));
