a570d1b24a7f7362250f830a11d27f4fb6247a68
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-11-26 13:45:43 +0530

    success

--- index.js
+++ index.js
@@ -5,24 +5,37 @@ var LinkedNode = /** @class */ (function () {
     }
     return LinkedNode;
 }());
-var reverseLinkedList = function (head) {
-    if (!head)
-        return head;
-    var prev = null;
-    var curr = head;
-    var forr = null;
-    while (curr) {
-        forr = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = forr;
+var intersectionLkNde = function (head1, head2) {
+    if (!head1 || !head2) {
+        return null;
     }
-    // console.log(prev);
-    head = prev;
-    return head;
+    var ptr1 = head1;
+    var ptr2 = head2;
+    while (ptr1 !== ptr2) {
+        ptr1 = ptr1.next;
+        ptr2 = ptr2.next;
+        if (ptr1 === ptr2) {
+            return ptr1;
+        }
+        if (!ptr1) {
+            ptr1 = head2;
+        }
+        if (!ptr2) {
+            ptr2 = head1;
+        }
+    }
+    return ptr1;
 };
-var head = new LinkedNode(1);
-head.next = new LinkedNode(2);
-head.next.next = new LinkedNode(3);
-head.next.next.next = new LinkedNode(4);
-console.log(reverseLinkedList(head));
+// let newNode:LinkedNode|null;
+var head1 = new LinkedNode(1);
+head1.next = new LinkedNode(2);
+head1.next.next = new LinkedNode(3);
+head1.next.next.next = new LinkedNode(4);
+head1.next.next.next.next = new LinkedNode(5);
+head1.next.next.next.next.next = new LinkedNode(6);
+var head2 = new LinkedNode(1.1);
+head2.next = new LinkedNode(2.2);
+head2.next.next = new LinkedNode(3.3);
+head2.next.next.next = new LinkedNode(4.4);
+head2.next.next.next.next = head1.next.next.next;
+console.log(intersectionLkNde(head1, head2));
