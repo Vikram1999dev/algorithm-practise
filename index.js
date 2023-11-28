b3cf6149ae261d60b3463b41cfeb70262f67100c
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-11-28 12:19:44 +0530

    success

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-var cyclicSort = function (arr) {
-    var _a;
-    var rightIndex;
-    for (var i = 0; i < arr.length;) {
-        rightIndex = arr[i] - 1;
-        if (arr[i] !== arr[rightIndex]) {
-            _a = [arr[rightIndex], arr[i]], arr[i] = _a[0], arr[rightIndex] = _a[1];
-        }
-        else {
-            i++;
-        }
+var IslandCounter = /** @class */ (function () {
+    function IslandCounter() {
     }
-    return arr;
-};
-console.log(cyclicSort([3, 5, 2, 1, 4]));
+    IslandCounter.prototype.explore = function (island, row, col) {
+        var h = island.length;
+        var w = island[0].length;
+        if (row < 0 || col < 0 || col >= w || row >= h || island[row][col] === 0) {
+            return;
+        }
+        island[row][col] = 0; // Mark the cell as visited
+        this.explore(island, row - 1, col);
+        this.explore(island, row + 1, col);
+        this.explore(island, row, col - 1);
+        this.explore(island, row, col + 1);
+    };
+    IslandCounter.prototype.countIslands = function (island) {
+        var h = island.length;
+        var w = island[0].length;
+        var result = 0;
+        for (var i = 0; i < h; i++) {
+            for (var j = 0; j < w; j++) {
+                if (island[i][j] === 1) {
+                    result++;
+                    this.explore(island, i, j);
+                }
+            }
+        }
+        return result;
+    };
+    return IslandCounter;
+}());
+// Example usage:
+var islandCounter = new IslandCounter();
+var islandGrid = [
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1],
+];
+var numberOfIslands = islandCounter.countIslands(islandGrid);
+console.log("Number of islands: ".concat(numberOfIslands));
--- index.ts
+++ index.ts
@@ -1,14 +1,46 @@
-const cyclicSort = (arr: number[]): number[] => {
-  let rightIndex: number;
-  for (let i: number = 0; i < arr.length; ) {
-    rightIndex = arr[i] - 1;
-    if (arr[i] !== arr[rightIndex]) {
-      [arr[i], arr[rightIndex]] = [arr[rightIndex], arr[i]];
-    } else {
-      i++;
-    }
-  }
-  return arr;
-};
+class IslandCounter {
+  private explore(island: number[][], row: number, col: number): void {
+    const h: number = island.length;
+    const w: number = island[0].length;
 
-console.log(cyclicSort([3, 5, 2, 1, 4]));
+    if (row < 0 || col < 0 || col >= w || row >= h || island[row][col] === 0) {
+      return;
+    }
+
+    island[row][col] = 0; // Mark the cell as visited
+    this.explore(island, row - 1, col);
+    this.explore(island, row + 1, col);
+    this.explore(island, row, col - 1);
+    this.explore(island, row, col + 1);
+  }
+
+  countIslands(island: number[][]): number {
+    const h: number = island.length;
+    const w: number = island[0].length;
+    let result: number = 0;
+
+    for (let i = 0; i < h; i++) {
+      for (let j = 0; j < w; j++) {
+        if (island[i][j] === 1) {
+          result++;
+          this.explore(island, i, j);
+        }
+      }
+    }
+
+    return result;
+  }
+}
+
+// Example usage:
+const islandCounter = new IslandCounter();
+
+const islandGrid: number[][] = [
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 1],
+];
+
+const numberOfIslands = islandCounter.countIslands(islandGrid);
+console.log(`Number of islands: ${numberOfIslands}`);
