77d0bef5a418ea3a4e6367dec62fc9fb4c818a34
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-11-28 12:08:27 +0530

    success

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-var mergeOverlappingIntervals = function (intervals) {
-    var result = [];
-    if (intervals.length <= 1) {
-        return intervals;
-    }
-    intervals.sort(function (a, b) { return a[0] - b[0]; });
-    var previous = intervals[0];
-    for (var i = 1; i < intervals.length; i++) {
-        if (previous[1] >= intervals[i][0]) {
-            previous = [
-                Math.min(previous[0], intervals[i][0]),
-                Math.max(previous[1], intervals[i][1]),
-            ];
+var cyclicSort = function (arr) {
+    var _a;
+    var rightIndex;
+    for (var i = 0; i < arr.length;) {
+        rightIndex = arr[i] - 1;
+        if (arr[i] !== arr[rightIndex]) {
+            _a = [arr[rightIndex], arr[i]], arr[i] = _a[0], arr[rightIndex] = _a[1];
         }
         else {
-            result.push(previous);
-            previous = intervals[i];
+            i++;
         }
     }
-    result.push(previous);
-    return result;
+    return arr;
 };
-var theArrs = [
-    [1, 3],
-    [9, 12],
-    [2, 6],
-    [8, 13],
-    [15, 18],
-];
-console.log(mergeOverlappingIntervals(theArrs));
+console.log(cyclicSort([3, 5, 2, 1, 4]));
--- index.ts
+++ index.ts
@@ -1,32 +1,14 @@
-var mergeOverlappingIntervals = (intervals: number[][]): number[][] => {
-  let result: number[][] = [];
-  if (intervals.length <= 1) {
-    return intervals;
-  }
-
-  intervals.sort((a, b) => a[0] - b[0]);
-  let previous: number[] = intervals[0];
-  for (let i = 1; i < intervals.length; i++) {
-    if (previous[1] >= intervals[i][0]) {
-      previous = [
-        Math.min(previous[0], intervals[i][0]),
-        Math.max(previous[1], intervals[i][1]),
-      ];
+const cyclicSort = (arr: number[]): number[] => {
+  let rightIndex: number;
+  for (let i: number = 0; i < arr.length; ) {
+    rightIndex = arr[i] - 1;
+    if (arr[i] !== arr[rightIndex]) {
+      [arr[i], arr[rightIndex]] = [arr[rightIndex], arr[i]];
     } else {
-      result.push(previous);
-      previous = intervals[i];
+      i++;
     }
   }
-  result.push(previous);
-  return result;
+  return arr;
 };
 
-let theArrs: number[][] = [
-  [1, 3],
-  [9, 12],
-  [2, 6],
-  [8, 13],
-  [15, 18],
-];
-
-console.log(mergeOverlappingIntervals(theArrs));
+console.log(cyclicSort([3, 5, 2, 1, 4]));
