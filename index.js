31db487224682edc1d28a9daa3773c672ee4cbef
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-06 12:36:33 +0530

    success

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-// you are given an array of integers, and the goal is
-// to find the longest subarray (contiguous subsequence)
-// whose elements sum up to a specific target value,
-// denoted as K.
-var lengthLongestSubarr = function (arr, k) {
-    var i = 0, j = 0;
-    var max = -Infinity;
-    var sum = 0;
-    while (j < arr.length) {
-        sum = sum + arr[j];
-        if (sum < k) {
-            j++;
-        }
-        else if (sum === k) {
-            max = Math.max(max, j - i + 1);
-            j++;
-        }
-        else if (sum > k) {
-            while (sum > k) {
-                sum = sum - arr[i];
-                i++;
-            }
-            j++;
-        }
+var TreeNode = /** @class */ (function () {
+    function TreeNode(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
-    return max;
+    return TreeNode;
+}());
+var findClosest = function (root, k, closest) {
+    if (!root) {
+        return closest;
+    }
+    if (Math.abs(k - closest) > Math.abs(k - root.data)) {
+        closest = root.data;
+    }
+    if (k > root.data) {
+        return findClosest(root.right, k, closest);
+    }
+    else if (k < root.data) {
+        return findClosest(root.left, k, closest);
+    }
+    else {
+        return closest;
+    }
 };
-var arr = [8, 6, 5, 2, 7, 1, 3, 1, 9];
-var k = 14;
-console.log(lengthLongestSubarr(arr, k));
+// Creating a BST with 15 nodes
+var root = new TreeNode(10);
+root.left = new TreeNode(5);
+root.right = new TreeNode(15);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(8);
+root.right.left = new TreeNode(12);
+root.right.right = new TreeNode(18);
+root.left.left.left = new TreeNode(2);
+root.left.left.right = new TreeNode(4);
+root.left.right.left = new TreeNode(7);
+root.left.right.right = new TreeNode(9);
+root.right.left.left = new TreeNode(11);
+root.right.left.right = new TreeNode(14);
+root.right.right.left = new TreeNode(17);
+root.right.right.right = new TreeNode(20);
+console.log(findClosest(root, 18, Infinity));
--- index.ts
+++ index.ts
@@ -1,31 +1,51 @@
-// you are given an array of integers, and the goal is
-// to find the longest subarray (contiguous subsequence)
-// whose elements sum up to a specific target value,
-// denoted as K.
-
-var lengthLongestSubarr = (arr: number[], k: number): number => {
-  let i: number = 0,
-    j: number = 0;
-  let max: number = -Infinity;
-  let sum: number = 0;
-  while (j < arr.length) {
-    sum = sum + arr[j];
-    if (sum < k) {
-      j++;
-    } else if (sum === k) {
-      max = Math.max(max, j - i + 1);
-      j++;
-    } else if (sum > k) {
-      while (sum > k) {
-        sum = sum - arr[i];
-        i++;
-      }
-      j++;
-    }
+class TreeNode {
+  data: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(data: number) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
-  return max;
+}
+
+const findClosest = (
+  root: TreeNode | null,
+  k: number,
+  closest: number,
+): number => {
+  if (!root) {
+    return closest;
+  }
+
+  if (Math.abs(k - closest) > Math.abs(k - root.data)) {
+    closest = root.data;
+  }
+
+  if (k > root.data) {
+    return findClosest(root.right, k, closest);
+  } else if (k < root.data) {
+    return findClosest(root.left, k, closest);
+  } else {
+    return closest;
+  }
 };
 
-let arr = [8, 6, 5, 2, 7, 1, 3, 1, 9];
-let k = 14;
-console.log(lengthLongestSubarr(arr, k));
+// Creating a BST with 15 nodes
+let root = new TreeNode(10);
+root.left = new TreeNode(5);
+root.right = new TreeNode(15);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(8);
+root.right.left = new TreeNode(12);
+root.right.right = new TreeNode(18);
+root.left.left.left = new TreeNode(2);
+root.left.left.right = new TreeNode(4);
+root.left.right.left = new TreeNode(7);
+root.left.right.right = new TreeNode(9);
+root.right.left.left = new TreeNode(11);
+root.right.left.right = new TreeNode(14);
+root.right.right.left = new TreeNode(17);
+root.right.right.right = new TreeNode(20);
+
+console.log(findClosest(root, 18, Infinity));
