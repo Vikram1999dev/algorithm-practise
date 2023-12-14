d704709102d11a80c1c126e970f0e62b7aaed686
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-14 10:53:34 +0530

    success

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-var caesarCipherEncryptor = function (word, key) {
-    var stringArr = [];
-    for (var i = 0; i < word.length; i++) {
-        var ccode = (word.charCodeAt(i) - 97 + key) % 26;
-        stringArr.push(String.fromCharCode(ccode + 97));
+//You are given a string of available characters and a
+//string representing a document that you need to generate
+//You are only able to generate the document if the frequency
+//of unique characters in the characters string is greater than or equal
+//to the frequency of unique characters in the document string
+var generateDocument = function (str, document) {
+    var maps = {};
+    for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
+        var char = s_1[_i];
+        maps[char] = (maps[char] || 0) + 1;
     }
-    return stringArr.join('');
+    console.log(maps);
+    for (var i = 0; i < document.length; i++) {
+        if (maps[document[i]]) {
+            if (maps[document[i]] === 0) {
+                return false;
+            }
+            maps[document]--;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 };
-var word = 'abc';
-var key = 5;
-console.log(caesarCipherEncryptor(word, key));
+var s = 'Bstehetsi ogEAxpelrt x ';
+var d = 'AlgoExpert is the Best!';
+console.log(generateDocument(s, d));
--- index.ts
+++ index.ts
@@ -1,13 +1,31 @@
-const caesarCipherEncryptor = (word: string, key: number): string => {
-  let stringArr: string[] = [];
-  for (let i: number = 0; i < word.length; i++) {
-    let ccode = (word.charCodeAt(i) - 97 + key) % 26;
-    stringArr.push(String.fromCharCode(ccode + 97));
+//You are given a string of available characters and a
+//string representing a document that you need to generate
+//You are only able to generate the document if the frequency
+//of unique characters in the characters string is greater than or equal
+//to the frequency of unique characters in the document string
+
+const generateDocument = (str: string, document: string): boolean => {
+  let maps: { [key: string]: number } = {};
+  for (let char of s) {
+    maps[char] = (maps[char] || 0) + 1;
   }
-  return stringArr.join('');
+
+  console.log(maps);
+
+  for (let i = 0; i < document.length; i++) {
+    if (maps[document[i]]) {
+      if (maps[document[i]] === 0) {
+        return false;
+      }
+      maps[document]--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-let word: string = 'abc';
-let key: number = 5;
-
-console.log(caesarCipherEncryptor(word, key));
+let s = 'Bste!hetsi ogEAxpelrt x ';
+let d = 'AlgoExpert is the Best!';
+console.log(generateDocument(s, d));
