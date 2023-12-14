36f69a12c12d784e55dde371bf249943366e16a2
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-14 11:17:09 +0530

    success

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-var firstRepeatingCharacter = function (str) {
-    var maps = {};
-    for (var i = 0; i < str.length; i++) {
-        maps[str[i]] = (maps[str[i]] || 0) + 1;
-    }
-    for (var i = 0; i < str.length; i++) {
-        if (maps[str[i]] === 1) {
-            return str[i];
+// Given an array of integers arr and a target sum target, implement a function
+// to find all unique triplets in the array that add up to the target sum. Return
+// a list of triplets. Each triplet should be a list of three integers.
+var threeNumberSum = function (arr, targetSum) {
+    arr.sort(function (a, b) { return a - b; });
+    var arrStack = [];
+    var l, r;
+    for (var i = 0; i < arr.length; i++) {
+        l = i + 1;
+        r = arr.length - 1;
+        while (l < r) {
+            var sum = arr[i] + arr[l] + arr[r];
+            if (sum == targetSum) {
+                arrStack.push([arr[i], arr[l], arr[r]]);
+                l++;
+                r--;
+            }
+            else if (sum < targetSum) {
+                l++;
+            }
+            else if (sum > targetSum) {
+                r--;
+            }
         }
     }
-    return '';
+    return arrStack;
 };
-var s = 'abcdcaf';
-console.log(firstRepeatingCharacter(s));
+var arr = [-7, 3, 4, 1, 3, -4, 4];
+console.log(threeNumberSum(arr, 0));
--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
-var firstRepeatingCharacter = (str: string): string => {
-  let maps: { [key: string]: number } = {};
-  for (let i: number = 0; i < str.length; i++) {
-    maps[str[i]] = (maps[str[i]] || 0) + 1;
-  }
+// Given an array of integers arr and a target sum target, implement a function
+// to find all unique triplets in the array that add up to the target sum. Return
+// a list of triplets. Each triplet should be a list of three integers.
 
-  for (let i: number = 0; i < str.length; i++) {
-    if (maps[str[i]] === 1) {
-      return str[i];
+const threeNumberSum = (arr: number[], targetSum: number): number[][] => {
+  arr.sort((a, b) => a - b);
+  let arrStack: number[][] = [];
+  let l: number, r: number;
+  for (let i: number = 0; i < arr.length; i++) {
+    l = i + 1;
+    r = arr.length - 1;
+    while (l < r) {
+      let sum = arr[i] + arr[l] + arr[r];
+      if (sum == targetSum) {
+        arrStack.push([arr[i], arr[l], arr[r]]);
+        l++;
+        r--;
+      } else if (sum < targetSum) {
+        l++;
+      } else if (sum > targetSum) {
+        r--;
+      }
     }
   }
-  return '';
+  return arrStack;
 };
 
-let s = 'abcdcaf';
-console.log(firstRepeatingCharacter(s));
+let arr = [-7, 3, 4, 1, 3, -4, 4];
+console.log(threeNumberSum(arr, 0));
